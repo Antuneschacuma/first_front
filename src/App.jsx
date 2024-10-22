ee800d02abdf7a1fa76b9d59809f81594a53f693
Antuneschacuma <antuneschacuma@gmail.com>
2024-10-22 10:56:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+
+function App() {
+ 
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className=" text-3xl text-slate-100 font-bold text-center">Gerenciar Tarefas</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
