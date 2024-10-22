570c70f7260f2a9188f1fd1c0305e93ba4c4f6b0
Antuneschacuma <antuneschacuma@gmail.com>
2024-10-22 13:43:12 +0100

    create Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import './index.css';
-
+import { useState } from "react";
+import "./index.css";
+import Tasks from "./components/Tasks";
+import AddTasks from "./components/AddTask";
 
 function App() {
- 
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "React",
+      descrption: "what a beautifull technology",
+      isComplete: false,
+    },
+    {
+      id: "2",
+      title: "Angular",
+      descrption: "what a beautifull Onother One",
+      isComplete: false,
+    },
+    {
+      id: "3",
+      title: "Vue",
+      descrption: "what a beautifull Third One",
+      isComplete: false,
+    },
+  ]);
 
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isComplete: !task.isComplete }; 
+      }
+      return task;
+    });
+    setTasks(newTask);
+  }
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
-        <h1 className=" text-3xl text-slate-100 font-bold text-center">Gerenciar Tarefas</h1>
+        <h1 className=" text-3xl text-slate-100 font-bold text-center">
+          Gerenciar Tarefas
+        </h1>
+        <AddTasks/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
     </div>
   );
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,30 @@
+import { ChevronsRightIcon, TrashIcon } from "lucide-react";
+
+function Tasks({tasks,onTaskClick,onDeleteTaskClick}) {
+  return (
+    <div>
+      <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+        {
+         tasks.map((task) => (
+            <li key={task.id} className="flex gap-2">
+              <button 
+                onClick={() =>onTaskClick(task.id)} 
+                className={`bg-slate-400 w-full text-white text-left p-2 rounded-md ${task.isComplete ? 'line-through' : ''}`}
+              >
+                {task.title}
+              </button>
+              <button className="bg-slate-400 text-white p-2 rounded-md">
+                <ChevronsRightIcon />
+              </button>
+              <button onClick={()=>props.onDeleteTaskClick(task.id)} className="bg-slate-400 text-white p-2 rounded-md">
+                <TrashIcon/>
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default Tasks;
